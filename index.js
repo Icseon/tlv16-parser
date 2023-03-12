acf97b5acaa5fb0b2c075bd41e9d00b4333007c8
Xander Sterrenburg <me@icseon.com>
2023-03-12 15:27:48 +0100

    Typo

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ module.exports = {
             offset += length;
 
             /* Compute full buffer */
-            const buffer = Buffer.concat([ Buffer.from(type), Buffer.from(length), value) ], length + 4);
+            const buffer = Buffer.concat([ Buffer.from(type), Buffer.from(length), value ], length + 4);
 
             /* Add to parsed array */
             parsed.push({ type, length, value, buffer });
